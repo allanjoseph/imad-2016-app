62c541c28bf919c5027d81fbf1af3d30c2c36c01
allanjoseph <allanjoseph97@gmail.com>
2016-09-25 07:17:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));});
+app.get('/article-one',function(req,res){res.send("article one requested and will be served soon");
 });
-app.get('/article-one',function(req,res){res.send("article one requested and will be served soon")
-});
-app.get('/article-two',function(req,res){res.send("article two is requested and will be served soon")
+app.get('/article-two',function(req,res){res.send("article two is requested and will be served soon");
     
 });
-app.get('/article-three',function(req,res){res.send("article three is requested and will be served soon")
+app.get('/article-three',function(req,res){res.send("article three is requested and will be served soon");
     
 });
 
